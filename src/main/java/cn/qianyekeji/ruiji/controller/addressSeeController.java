af459494b576a46fc7335e7de4ac65aec0d7dbb5
liangshuai <415264996@qq.com>
2023-03-19 18:03:58 +0800

    瑞吉外卖-匿名群聊-bug

--- src/main/java/cn/qianyekeji/ruiji/controller/addressSeeController.java
+++ src/main/java/cn/qianyekeji/ruiji/controller/addressSeeController.java
@@ -106,99 +106,99 @@ public class addressSeeController {
             long score = zSetOps.score(key, element).longValue();
             int timeDiff = getTimeDiff(score);
 
-            String s="";
-            if (timeDiff!=0) {
+            String s = "";
+            if (timeDiff != 0) {
                 //炼气期一天升一级
-                if (timeDiff>=1&&timeDiff<=9){
-                    s=arr0[timeDiff-1];
+                if (timeDiff >= 1 && timeDiff <= 9) {
+                    s = arr0[timeDiff - 1];
                 }
                 //筑基期10天升一级
-                if (timeDiff>9&&timeDiff<=19){
-                    s=arr1[0];
+                if (timeDiff > 9 && timeDiff <= 19) {
+                    s = arr1[0];
                 }
-                if (timeDiff>19&&timeDiff<=29){
-                    s=arr1[1];
+                if (timeDiff > 19 && timeDiff <= 29) {
+                    s = arr1[1];
                 }
-                if (timeDiff>29&&timeDiff<=39){
-                    s=arr1[2];
+                if (timeDiff > 29 && timeDiff <= 39) {
+                    s = arr1[2];
                 }
-                if (timeDiff>39&&timeDiff<=49){
-                    s=arr1[3];
+                if (timeDiff > 39 && timeDiff <= 49) {
+                    s = arr1[3];
                 }
-                if (timeDiff>49&&timeDiff<=59){
-                    s=arr1[4];
+                if (timeDiff > 49 && timeDiff <= 59) {
+                    s = arr1[4];
                 }
-                if (timeDiff>59&&timeDiff<=69){
-                    s=arr1[5];
+                if (timeDiff > 59 && timeDiff <= 69) {
+                    s = arr1[5];
                 }
-                if (timeDiff>69&&timeDiff<=79){
-                    s=arr1[6];
+                if (timeDiff > 69 && timeDiff <= 79) {
+                    s = arr1[6];
                 }
-                if (timeDiff>79&&timeDiff<=89){
-                    s=arr1[7];
+                if (timeDiff > 79 && timeDiff <= 89) {
+                    s = arr1[7];
                 }
-                if (timeDiff>89&&timeDiff<=99){
-                    s=arr1[8];
+                if (timeDiff > 89 && timeDiff <= 99) {
+                    s = arr1[8];
                 }
                 //金丹期10天升一级
-                if (timeDiff>99&&timeDiff<=109){
-                    s=arr2[0];
+                if (timeDiff > 99 && timeDiff <= 109) {
+                    s = arr2[0];
                 }
-                if (timeDiff>109&&timeDiff<=119){
-                    s=arr2[1];
+                if (timeDiff > 109 && timeDiff <= 119) {
+                    s = arr2[1];
                 }
-                if (timeDiff>119&&timeDiff<=129){
-                    s=arr2[2];
+                if (timeDiff > 119 && timeDiff <= 129) {
+                    s = arr2[2];
                 }
-                if (timeDiff>129&&timeDiff<=139){
-                    s=arr2[3];
+                if (timeDiff > 129 && timeDiff <= 139) {
+                    s = arr2[3];
                 }
-                if (timeDiff>139&&timeDiff<=149){
-                    s=arr2[4];
+                if (timeDiff > 139 && timeDiff <= 149) {
+                    s = arr2[4];
                 }
-                if (timeDiff>149&&timeDiff<=159){
-                    s=arr2[5];
+                if (timeDiff > 149 && timeDiff <= 159) {
+                    s = arr2[5];
                 }
-                if (timeDiff>159&&timeDiff<=169){
-                    s=arr2[6];
+                if (timeDiff > 159 && timeDiff <= 169) {
+                    s = arr2[6];
                 }
-                if (timeDiff>169&&timeDiff<=179){
-                    s=arr2[7];
+                if (timeDiff > 169 && timeDiff <= 179) {
+                    s = arr2[7];
                 }
-                if (timeDiff>179&&timeDiff<=189){
-                    s=arr2[8];
+                if (timeDiff > 179 && timeDiff <= 189) {
+                    s = arr2[8];
                 }
                 //化神期10天升一级
-                if (timeDiff>189&&timeDiff<=199){
-                    s=arr3[0];
+                if (timeDiff > 189 && timeDiff <= 199) {
+                    s = arr3[0];
                 }
-                if (timeDiff>199&&timeDiff<=209){
-                    s=arr3[1];
+                if (timeDiff > 199 && timeDiff <= 209) {
+                    s = arr3[1];
                 }
-                if (timeDiff>209&&timeDiff<=219){
-                    s=arr3[2];
+                if (timeDiff > 209 && timeDiff <= 219) {
+                    s = arr3[2];
                 }
-                if (timeDiff>219&&timeDiff<=229){
-                    s=arr3[3];
+                if (timeDiff > 219 && timeDiff <= 229) {
+                    s = arr3[3];
                 }
-                if (timeDiff>229&&timeDiff<=239){
-                    s=arr3[4];
+                if (timeDiff > 229 && timeDiff <= 239) {
+                    s = arr3[4];
                 }
-                if (timeDiff>239&&timeDiff<=249){
-                    s=arr3[5];
+                if (timeDiff > 239 && timeDiff <= 249) {
+                    s = arr3[5];
                 }
-                if (timeDiff>249&&timeDiff<=259){
-                    s=arr3[6];
+                if (timeDiff > 249 && timeDiff <= 259) {
+                    s = arr3[6];
                 }
-                if (timeDiff>259&&timeDiff<=269){
-                    s=arr3[7];
+                if (timeDiff > 259 && timeDiff <= 269) {
+                    s = arr3[7];
                 }
-                if (timeDiff>269&&timeDiff<=279){
-                    s=arr3[8];
+                if (timeDiff > 269 && timeDiff <= 279) {
+                    s = arr3[8];
                 }
                 //神
-                if (timeDiff>279){
-                    s=arr4[0];
+                if (timeDiff > 279) {
+                    s = arr4[0];
                 }
                 String[] data = element.split("__");
                 if (data.length != 2) {
@@ -206,7 +206,7 @@ public class addressSeeController {
                 }
                 String latitude = data[0];
                 String longitude = data[1];
-                Address address = new Address(latitude, longitude, s+"道友");
+                Address address = new Address(latitude, longitude, s + "道友");
                 addresses.add(address);
             }
         }
@@ -225,19 +225,22 @@ public class addressSeeController {
     }
 
 
-
-    @PostMapping("/{uuid}")
-    public R<Long> number(@PathVariable("uuid") String uuid) {
-        if (uuid==null) {
-            uuid="1";
+    @PostMapping("/{uuid}/{uuid2}")
+    public R<Long> number(@PathVariable("uuid") String uuid, @PathVariable("uuid2") String uuid2) {
+//        System.out.println(uuid + "-----------------" + uuid2);
+        if (uuid == null) {
+            uuid = "1";
+        }
+        if (uuid2 == null) {
+            uuid2 = "_2";
         }
         // 将 uuid 和时间戳信息存储到 Redis 中
         long timestamp = Instant.now().toEpochMilli(); // 获取当前时间的时间戳
-        redisTemplate.opsForZSet().add("lswc", uuid, timestamp);
+        redisTemplate.opsForZSet().add("lswc", uuid + "_" + uuid2, timestamp);
 
         // 返回当前 uuid 对应的次数
         Long count = redisTemplate.opsForZSet().zCard("lswc");
-        count=count==null?0:count;
+        count = count == null ? 0 : count;
         return R.success(count);
     }
 
@@ -288,9 +291,12 @@ public class addressSeeController {
 //        return null;
 //    }
 
-    @PostMapping("/address/{address}")
-    public R<List<GeoResult<RedisGeoCommands.GeoLocation<String>>>> address(@PathVariable("address") String address) {
-        System.out.println(address+"------------------------");
+    @PostMapping("/address/{address}/{uuid2}")
+    public R<List<GeoResult<RedisGeoCommands.GeoLocation<String>>>> address(@PathVariable("address") String address, @PathVariable("uuid2") String uuid2) {
+//        System.out.println(address + "------------------------" + uuid2);
+        if (uuid2 == null) {
+            uuid2 = "_2";
+        }
         //获取传入的经纬度
         String[] coordinates = address.split("__");
         String longitude = coordinates[0];
@@ -306,31 +312,30 @@ public class addressSeeController {
         if (!onlineUsers.isEmpty()) {
             Map<String, Point> members = new HashMap<>();
             for (String user : onlineUsers) {
-                String[] userCoordinates = user.split("__");
+                String userWithoutEnding = user.substring(0, user.lastIndexOf("_"));
+                String[] userCoordinates = userWithoutEnding.split("__");
                 String userLongitude = userCoordinates[0];
                 String userLatitude = userCoordinates[1];
                 Point userPoint = new Point(Double.parseDouble(userLongitude), Double.parseDouble(userLatitude));
-                members.put(user, userPoint);
+                members.put(userWithoutEnding, userPoint);
             }
             redisTemplate.opsForGeo().add(key, members);
         }
-
         // 再将传入的经纬度存储到Redis的Geo数据类型中
         redisTemplate.opsForGeo().add(key, point, member);
-
         //这时候geo数据类型中有两部分，一部分是传入的经纬度
         // 另一部分是在线人员(这里的在线人员指的只是大厅里的人员)的经纬度
-
-        //计算传入的经纬度和其他在线人员的经纬度之间的距离，并返回距离列表
         Circle circle = new Circle(point, new Distance(3000, Metrics.KILOMETERS));
         GeoResults<RedisGeoCommands.GeoLocation<String>> geoResults = redisTemplate.opsForGeo().radius(key, circle);
         List<GeoResult<RedisGeoCommands.GeoLocation<String>>> resultList = new ArrayList<>();
         for (GeoResult<RedisGeoCommands.GeoLocation<String>> geoResult : geoResults) {
             RedisGeoCommands.GeoLocation<String> location = geoResult.getContent();
-            double distance = redisTemplate.opsForGeo().distance(key, member, location.getName(), Metrics.KILOMETERS).getValue();
-            resultList.add(new GeoResult<>(location, new Distance(distance, Metrics.KILOMETERS)));
+            // 判断当前遍历到的location是否是传入的经纬度
+            if (!location.getName().equals(member)) {
+                double distance = redisTemplate.opsForGeo().distance(key, member, location.getName(), Metrics.KILOMETERS).getValue();
+                resultList.add(new GeoResult<>(location, new Distance(distance, Metrics.KILOMETERS)));
+            }
         }
-
         System.out.println(resultList);
         //清空geo数据类型中的所有内容
         redisTemplate.delete(key);
