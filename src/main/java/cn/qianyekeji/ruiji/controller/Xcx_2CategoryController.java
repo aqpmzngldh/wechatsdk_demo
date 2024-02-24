6ae19ea9f2ce2c9a79fd3b572c4f4dd9a41e4a3b
liangdachui <ls@qianyekeji.cn>
2024-02-25 01:18:06 +0800

    图片上传

--- src/main/java/cn/qianyekeji/ruiji/controller/Xcx_2CategoryController.java
+++ src/main/java/cn/qianyekeji/ruiji/controller/Xcx_2CategoryController.java
@@ -8,17 +8,24 @@ import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
 @RestController
-@RequestMapping("xcx_2/category")
+@RequestMapping("/xcx_2")
 @Slf4j
 public class Xcx_2CategoryController {
     @Autowired
     private Xcx_2CategoryService xcx_2CategoryService;
-    @GetMapping("/addCategory")
+    @Value("${ruiji.path2}")
+    private String basePath;
+
+    @GetMapping("/category/addCategory")
     public R<String> list(Xcx_2Category xcx_2Category) {
         System.out.println("传递过来的分类名称是"+xcx_2Category);
         QueryWrapper<Xcx_2Category> queryWrapper = new QueryWrapper<>();
@@ -38,7 +45,7 @@ public class Xcx_2CategoryController {
 //        System.out.println(list);
 //        return R.success(list);
 //    }
-    @GetMapping("/getCategory")
+    @GetMapping("/category/getCategory")
     public R<Page> page(int page, int pageSize){
         //分页构造器
         Page<Xcx_2Category> pageInfo = new Page<>(page,pageSize);
@@ -50,7 +57,7 @@ public class Xcx_2CategoryController {
         return R.success(pageInfo);
     }
 
-    @PostMapping("/deleteCategory")
+    @PostMapping("/category/deleteCategory")
     public R<String> delete(Xcx_2Category xcx_2Category){
         log.info("删除分类，分类为：{}",xcx_2Category.getSortName());
         System.out.println("删除的分类是"+xcx_2Category.getSortName());
@@ -60,5 +67,34 @@ public class Xcx_2CategoryController {
         // 根据sortName作为条件构造器删除
         xcx_2CategoryService.remove(wrapper);
         return R.success("分类信息删除成功");
+    }
+
+    @PostMapping("/upload")
+    public String uploadFile(@RequestParam("file") MultipartFile file) {
+//        String sss="F:\\www\\server\\img2\\";
+        // 获取上传的文件
+        if (file.isEmpty()) {
+            System.out.println("上传的文件为空");
+        }else{
+            System.out.println("上传的文件不不不为空");
+        }
+        String originalFilename = file.getOriginalFilename();
+        log.info("传递过来的文件名为，{}",originalFilename);
+
+        // 创建目标文件对象
+//        File dest = new File(sss + originalFilename);
+        File dest = new File(basePath + originalFilename);
+        if (!dest.exists()) {
+            dest.mkdirs();
+        }
+        try {
+            // 将上传的文件保存到服务器
+            file.transferTo(dest);
+//            System.out.println("文件上传成功，保存路径为：" + sss + originalFilename);
+            System.out.println("文件上传成功，保存路径为：" + basePath + originalFilename);
+        } catch (IOException e) {
+            System.out.println("文件上传失败: " + e.getMessage());
+        }
+        return "https://qianyekeji.cn/img2/"+originalFilename;
     }
 }
