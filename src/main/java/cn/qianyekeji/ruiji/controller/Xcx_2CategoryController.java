136d97c6e0d8bc6e94e9cbbc26ffb3b6d3f09048
liangdachui <ls@qianyekeji.cn>
2024-02-29 20:45:21 +0800

    商品数据的提交

--- src/main/java/cn/qianyekeji/ruiji/controller/Xcx_2CategoryController.java
+++ src/main/java/cn/qianyekeji/ruiji/controller/Xcx_2CategoryController.java
@@ -2,9 +2,12 @@ package cn.qianyekeji.ruiji.controller;
 
 import cn.qianyekeji.ruiji.common.R;
 import cn.qianyekeji.ruiji.entity.Xcx_2Category;
+import cn.qianyekeji.ruiji.entity.Xcx_2Goods;
 import cn.qianyekeji.ruiji.service.Xcx_2CategoryService;
+import cn.qianyekeji.ruiji.service.Xcx_2GoodsService;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +18,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/xcx_2")
@@ -22,6 +26,8 @@ import java.util.List;
 public class Xcx_2CategoryController {
     @Autowired
     private Xcx_2CategoryService xcx_2CategoryService;
+    @Autowired
+    private Xcx_2GoodsService xcx_2GoodsService;
     @Value("${ruiji.path2}")
     private String basePath;
 
@@ -71,7 +77,7 @@ public class Xcx_2CategoryController {
 
     @PostMapping("/upload")
     public String uploadFile(@RequestParam("file") MultipartFile file) {
-//        String sss="F:\\www\\server\\img2\\";
+        String sss="F:\\www\\server\\img2\\";
         // 获取上传的文件
         if (file.isEmpty()) {
             System.out.println("上传的文件为空");
@@ -82,16 +88,16 @@ public class Xcx_2CategoryController {
         log.info("传递过来的文件名为，{}",originalFilename);
 
         // 创建目标文件对象
-//        File dest = new File(sss + originalFilename);
-        File dest = new File(basePath + originalFilename);
+        File dest = new File(sss + originalFilename);
+//        File dest = new File(basePath + originalFilename);
         if (!dest.exists()) {
             dest.mkdirs();
         }
         try {
             // 将上传的文件保存到服务器
             file.transferTo(dest);
-//            System.out.println("文件上传成功，保存路径为：" + sss + originalFilename);
-            System.out.println("文件上传成功，保存路径为：" + basePath + originalFilename);
+            System.out.println("文件上传成功，保存路径为：" + sss + originalFilename);
+//            System.out.println("文件上传成功，保存路径为：" + basePath + originalFilename);
         } catch (IOException e) {
             System.out.println("文件上传失败: " + e.getMessage());
         }
@@ -104,15 +110,56 @@ public class Xcx_2CategoryController {
         System.out.println(list);
         return R.success(list);
     }
-//    @PostMapping("/addGoods")
-////    public String addGoods(String goods_title,@RequestBody String[] goods_banner,String goods_cover,String video_url) {
-//    public String addGoods(String obj) {
-////        System.out.println(goods_title);
-////        System.out.println(goods_banner);
-////        System.out.println(goods_cover);
-////        System.out.println(video_url);
-//
-//        System.out.println(obj);
-//        return "333";
-//    }
+    @PostMapping("/addGoods")
+    public String addGoods(Xcx_2Goods xcx_2Goods){
+        System.out.println(xcx_2Goods);
+
+        Xcx_2Goods xcx_2Goods1 = new Xcx_2Goods();
+        xcx_2Goods1.setGoods_title(xcx_2Goods.getGoods_title());
+        xcx_2Goods1.setGoods_banner(xcx_2Goods.getGoods_banner());
+        xcx_2Goods1.setGoods_cover(xcx_2Goods.getGoods_cover());
+        xcx_2Goods1.setVideo_url(xcx_2Goods.getVideo_url());
+        xcx_2Goods1.setCategory(xcx_2Goods.getCategory());
+        xcx_2Goods1.setGoods_price(xcx_2Goods.getGoods_price());
+        xcx_2Goods1.setStock(xcx_2Goods.getStock());
+        xcx_2Goods1.setSku(xcx_2Goods.getSku());
+        xcx_2Goods1.setGoods_details(xcx_2Goods.getGoods_details());
+        xcx_2Goods1.setSold(xcx_2Goods.getSold());
+        xcx_2Goods1.setShelves(xcx_2Goods.getShelves());
+        xcx_2Goods1.setSeckill(xcx_2Goods.getSeckill());
+        // 插入数据
+        boolean success =xcx_2GoodsService.save(xcx_2Goods1);
+        // 获取插入的数据的ID
+        Long insertedId = xcx_2Goods1.getId();
+        System.out.println("Inserted ID: " + insertedId);
+        if (success) {
+            System.out.println("插入数据成功");
+            //获取分类下的quantity的数量
+            LambdaQueryWrapper<Xcx_2Category> wrapper = new LambdaQueryWrapper<>();
+            wrapper.eq(Xcx_2Category::getSortName, xcx_2Goods1.getCategory());
+            Xcx_2Category one = xcx_2CategoryService.getOne(wrapper);
+            if (one==null){
+                return "该分类已被删除，请重新添加分类";
+            }
+            String quantity = one.getQuantity();
+            //进行对quantity的数量的更新
+            UpdateWrapper<Xcx_2Category> updateWrapper = new UpdateWrapper<>();
+            updateWrapper.eq("sort_name", xcx_2Goods.getCategory()); // 设置更新的条件
+            Xcx_2Category user = new Xcx_2Category();
+
+            System.out.println(quantity);
+            System.out.println(String.valueOf(Integer.parseInt(quantity)+1));// 设置要更新的字段的新值
+            if (quantity!=null) {
+                user.setQuantity(String.valueOf(Integer.parseInt(quantity)+1)); // 设置要更新的字段的新值
+            }else{
+                user.setQuantity("1");
+            }
+            xcx_2CategoryService.update(user, updateWrapper); // 根据条件更新数据
+        } else {
+            System.out.println("插入数据失败");
+        }
+
+        return String.valueOf(insertedId);
+    }
+
 }
---
+++ src/main/java/cn/qianyekeji/ruiji/service/Xcx_2GoodsService.java
@@ -0,0 +1,9 @@
+package cn.qianyekeji.ruiji.service;
+
+import cn.qianyekeji.ruiji.entity.Xcx_2Category;
+import cn.qianyekeji.ruiji.entity.Xcx_2Goods;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+public interface Xcx_2GoodsService extends IService<Xcx_2Goods> {
+
+}
