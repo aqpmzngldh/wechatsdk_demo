d3cdae8c4ddaf688b420a6f80e595bd344bed12a
liangdachui <ls@qianyekeji.cn>
2024-06-10 16:39:43 +0800

    1

--- src/main/java/cn/qianyekeji/ruiji/controller/Xcx_2CategoryController.java
+++ src/main/java/cn/qianyekeji/ruiji/controller/Xcx_2CategoryController.java
@@ -30,10 +30,7 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.HashOperations;
