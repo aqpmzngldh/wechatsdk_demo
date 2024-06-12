c8f6875aaece2f4dd2a7eb24c34269185807a13f
liangdachui <ls@qianyekeji.cn>
2024-06-12 10:54:00 +0800

    目录结构改变

--- src/main/java/cn/qianyekeji/ruiji/service/Wx_voiceService.java
+++
@@ -1,8 +0,0 @@
-package cn.qianyekeji.ruiji.service;
-
-import cn.qianyekeji.ruiji.entity.WxVoice;
-import com.baomidou.mybatisplus.extension.service.IService;
-
-public interface Wx_voiceService extends IService<WxVoice> {
-
-}
