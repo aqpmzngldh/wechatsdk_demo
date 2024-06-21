890c655405d2796de0683fcf482dc9111b153167
aqpmzngldh <124235388+aqpmzngldh@users.noreply.github.com>
2024-06-21 14:45:14 +0800

    Update ChatgptService.java

--- src/main/java/cn/qianyekeji/wechatsdk/service/ChatgptService.java
+++ src/main/java/cn/qianyekeji/wechatsdk/service/ChatgptService.java
@@ -5,5 +5,5 @@ import cn.qianyekeji.wechatsdk.entity.ChatgptRequest;
 import com.baomidou.mybatisplus.extension.service.IService;
 
 public interface ChatgptService extends IService<ChatgptRequest> {
-    String chat(String userId, String message);
+    String chat(String userId, String message,String tag);
 }
