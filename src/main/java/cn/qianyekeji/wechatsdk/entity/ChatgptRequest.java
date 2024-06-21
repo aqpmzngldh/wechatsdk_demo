e8fe4d22cf04eaad004a3ef20a6477e130ab1fce
aqpmzngldh <124235388+aqpmzngldh@users.noreply.github.com>
2024-06-21 14:43:27 +0800

    Update ChatgptRequest.java

--- src/main/java/cn/qianyekeji/wechatsdk/entity/ChatgptRequest.java
+++ src/main/java/cn/qianyekeji/wechatsdk/entity/ChatgptRequest.java
@@ -1,22 +1,8 @@
 package cn.qianyekeji.wechatsdk.entity;
 
+@Data
 public class ChatgptRequest {
     private String userId;
     private String message;
-
-    public String getUserId() {
-        return userId;
-    }
-
-    public void setUserId(String userId) {
-        this.userId = userId;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
+    private String tag;
 }
